3856997fff84d1ff43a6e4dcc712324c0fc00b70
dotuanthanh <76996257+dotuanthanh@users.noreply.github.com>
2021-02-04 14:08:28 +0700

    first

---
+++ showDate.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class showDate{
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
